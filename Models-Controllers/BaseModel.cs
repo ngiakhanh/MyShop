1986694c25bd22593c956e3e06e8905a1fea813a
ngiakhanh <ngiakhanh96@gmail.com>
2018-08-14 22:49:31 +0700

    Init commit

---
+++ Models-Controllers/BaseModel.cs
@@ -0,0 +1,24 @@
+﻿using Models_Controllers.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models_Controllers
+{
+    public class BaseModels<T>
+    {
+        protected IMIC_SHOPEntities dbContext;
+        public BaseModels()
+        {
+            dbContext = new IMIC_SHOPEntities();
+        }
+
+        public virtual List<T> getElements() { return null; }
+        public virtual T getElementById(Guid id) { return default(T); }
+        public virtual Boolean create(T obj) { return true; }
+        public virtual Boolean update(T obj) { return true; }
+        public virtual Boolean delete(Guid id) { return true; }
+    }
+}
